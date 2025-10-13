284c75b084db61f8868fc1444437ba072d05d517
tbzz83 <tbzz83@gmail.com>
2025-10-13 08:04:15 -0400

    execve() demonstration

---
+++ processes/exec.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main() {
+  char *arg_vec[10];
+  char *env_vec[1];
+
+  // Needs to be a NULL terminated string.
+  arg_vec[1] = NULL;
+  env_vec[0] = NULL;
+
+
+  printf("Executing simple_writee.sh with spelling error\n");
+  // Trying to call non-existent program! (note spelling error)
+  if (execve("./simple_writee.sh", arg_vec, env_vec) == -1) {
+    // This will print!
+    printf("simple_writee.sh error\n");
+  }
+
+  printf("Executing simple_write.sh without spelling error\n");
+  // Now the correct program
+  if (execve("./simple_write.sh", arg_vec, env_vec) == -1) {
+    printf("simple_write.sh error\n");
+    //return EXIT_FAILURE;
+  }
+
+  // This text doesn't print, because execve loads the program
+  // requested, and never returns if successful.
+  printf("Text after execve\n");
+
+  return EXIT_SUCCESS;
+}
---
+++ processes/simple_write.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "hello world" > open_me.txt
+
+echo
+echo "Hello from simple_write.sh script!"
+echo "Succesfully executed $0"
+echo "Bye!"
