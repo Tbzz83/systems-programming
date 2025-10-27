bfd8a708362e73419fc87bb0ac3b53862965acd4
tbzz83 <tbzz83@gmail.com>
2025-10-27 10:46:39 -0400

    Sending signals

---
+++ signals/sending_signals.c
@@ -0,0 +1,43 @@
+#include <errno.h>
+#include <signal.h>
+#include <wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void spin_forever() {
+    while (1) {}
+}
+
+int main() {
+
+    pid_t child_id = fork();
+
+    switch (child_id) {
+        case 0:
+            printf("Hello from the child. My pid is %d\n", getpid());
+            spin_forever();
+        default:
+            printf("Hello from the parent. My pid is %d and my childs pid is %d\n", getpid(), child_id);
+            break;
+    }
+    printf("Checking if pid %d exists\n", child_id);
+    // Set child_id to something fun here to see what happens if the process doesn't exist.
+    if (kill(child_id, 0) == -1) {
+        printf("pid %d does not exist. errno %d. Terminating...\n", child_id, errno);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("pid %d exists! Issuing kill signal...\n", child_id);
+    kill(child_id, SIGKILL);
+
+    int child_status;
+    if (wait(&child_status) == -1) {
+        printf("Child failed. Exit status was %d\n", WEXITSTATUS(child_status));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("\n---\nChild collected!\nChild pid = %d\nExit status = %d\n", child_id, WEXITSTATUS(child_status));
+    
+    return EXIT_SUCCESS;
+}
--- signals/simple_sig.c
+++ signals/simple_sig.c
@@ -4,21 +4,37 @@
 #include <unistd.h>
 
 static void sig_handler(int sig) {
-    printf("\nGoodbye from signal handler :3 <3\n"); // UNSAFE
-    exit(EXIT_SUCCESS);
+    switch (sig) {
+    case SIGINT:
+        printf("\nCaught SIGINT (Ctrl+C)\n");
+        printf("resuming...\n");
+        return;
+    case SIGQUIT:
+        printf("\nCaught SIGQUIT (Ctrl+\\)\n");
+        printf("Goodbye from signal handler :3 <3\n"); // UNSAFE
+        exit(EXIT_SUCCESS);
+    }
 }
 
 int main() {
 
     int i;
 
+    // Allowing our signal_handler to handle multiple signals
     if (signal(SIGINT, &sig_handler) == SIG_ERR) {
         printf("signal failed.\n");
         exit(EXIT_FAILURE);
     }
 
+    if (signal(SIGQUIT, &sig_handler) == SIG_ERR) {
+        printf("signal failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("Press 'Ctrl+C' to interrupt, and 'Ctrl+\\' to quit\n");
     for (i = 0; ;i++) {
-        printf("%d: Please terminate me... (ctrl+c)\n", i);
+        printf("%d: Please interrupt or quit...\n", i);
         sleep(3);
     }
 
